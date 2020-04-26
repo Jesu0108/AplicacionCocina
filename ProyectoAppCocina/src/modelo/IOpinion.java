2ed381655472fd92c51f65dbda1b0368db5fd988
Jesu0108 <jartiguez2@gmail.com>
2020-04-26 15:55:36 +0200

    Creacion del toString y hashCode de la clase Opinion

--- ProyectoAppCocina/src/modelo/IOpinion.java
+++ ProyectoAppCocina/src/modelo/IOpinion.java
@@ -4,8 +4,6 @@ public interface IOpinion {
 
 	public int getiId_opinion();
 
-	public boolean setiId_opinion(int iId_opinion);
-
 	public String getsCritica();
 
 	public boolean setsCritica(String sCritica);
--- ProyectoAppCocina/src/modelo/Opinion.java
+++ ProyectoAppCocina/src/modelo/Opinion.java
@@ -16,8 +16,7 @@ public class Opinion implements IOpinion{
 		return iId_opinion;
 	}
 	
-	@Override
-	public boolean setiId_opinion(int iId_opinion) {
+	private boolean setiId_opinion(int iId_opinion) {
 		boolean bExito=false;
 		if(iId_opinion>-1) {
 			this.iId_opinion = iId_opinion;
@@ -54,6 +53,34 @@ public class Opinion implements IOpinion{
 			bExito=true;
 		}
 		bExito = true;
+		return bExito;
+	}
+
+	@Override
+	public String toString() {
+		String sResultado="";
+		sResultado+= "Puntuacion: "+getiPuntuacion();
+		sResultado+="Critica: "+getsCritica();
+		return sResultado;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + iId_opinion;
+		result = prime * result + iPuntuacion;
+		result = prime * result + ((sCritica == null) ? 0 : sCritica.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean bExito = false;
+		Opinion other = (Opinion) obj;
+		if(this.iId_opinion== other.iId_opinion) {
+		    bExito = true;
+		}
 		return bExito;
 	}
 	
