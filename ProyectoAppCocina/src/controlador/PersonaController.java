bda7a60689a508d80181b86b6a7d13f012f493ff
Jesu0108 <jartiguez2@gmail.com>
2020-04-30 14:23:30 +0200

    Se ha creado el controlador de la clase Persona, se han añadido tambien los metodos del mismo

---
+++ ProyectoAppCocina/src/controlador/PersonaController.java
@@ -0,0 +1,30 @@
+package controlador;
+
+import modelo.usuario.Persona;
+
+public class PersonaController {
+
+	//Operaciones CRUD
+	
+	public int add (Persona oPersona) {
+		String sql = "INSERT INTO persona VALUES (\""+oPersona.getsEmail()+"\",\""+oPersona.getsContrasenia()+"\")";
+		return ConexionDB.executeUpdate(sql);
+	}
+	
+	public int remove (Persona oPersona) {
+		String sql = "DELETE FROM persona WHERE email LIKE \""+oPersona.getsEmail()+"\"";
+		return ConexionDB.executeUpdate(sql);
+	}
+	
+	//Operaciones de checking
+	
+	public int existeUsuario (Persona oPersona) {
+		String sql = "SELECT COUNT(*) FROM persona WHERE email LIKE \"" + oPersona.getsEmail() + "\"";
+		return ConexionDB.executeCount(sql);
+	    }
+	
+	 public int checkLogin(Persona oPersona) {	
+			String sql = "SELECT COUNT(*) FROM persona WHERE email LIKE \"" + oPersona.getsEmail() + "\" AND contraseña LIKE \"" + oPersona.getsContrasenia() + "\"";	
+			return ConexionDB.executeCount(sql);
+		    }
+}
