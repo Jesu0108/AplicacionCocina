d32362495934506832771a0871e5dea2c88b7ca9
Jesu0108 <jartiguez2@gmail.com>
2020-05-04 14:04:24 +0200

    Se ha creado los metodos de la clase Cociero_x_servicio

--- ProyectoAppCocina/src/modelo/intermedias/Cocinero_x_servicio.java
+++ ProyectoAppCocina/src/modelo/intermedias/Cocinero_x_servicio.java
@@ -1,5 +1,56 @@
 package modelo.intermedias;
 
-public class Cocinero_x_servicio {
+import modelo.usuario.Cocinero;
+import modelo.usuario.Servicio;
 
+public class Cocinero_x_servicio {
+	private Cocinero email; //PK and FK
+	private Servicio id_servicio; //PK and FK
+	
+	public Cocinero_x_servicio(Cocinero email, Servicio id_servicio) {
+		setEmail(email);
+		setId_servicio(id_servicio);
+	}
+	
+	//Getters and Setters
+	
+	public Cocinero getEmail() {
+		return email;
+	}
+	
+	private void setEmail(Cocinero email) {
+		this.email = email;
+	}
+	
+	public Servicio getId_servicio() {
+		return id_servicio;
+	}
+	
+	private void setId_servicio(Servicio id_servicio) {
+		this.id_servicio = id_servicio;
+	}
+
+	//-----------------------------------------------------------------------------
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id_servicio == null) ? 0 : id_servicio.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean bExito = false;
+		Cocinero_x_servicio other = (Cocinero_x_servicio) obj;
+		if(this.email.equals(other.email) && this.id_servicio.equals(other.id_servicio)) {
+		    bExito = true;
+		}
+		return bExito;
+	}
+	
+	
+	
 }
