4dd23c5093e34879501a446fc457d9cecee099ea
Jesu0108 <jartiguez2@gmail.com>
2020-05-04 14:50:44 +0200

    Se ha añadido la funcion de busqueda en la clase Opinion

--- ProyectoAppCocina/src/controlador/usuario/OpinionController.java
+++ ProyectoAppCocina/src/controlador/usuario/OpinionController.java
@@ -1,5 +1,10 @@
 package controlador.usuario;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import controlador.ConexionDB;
 import modelo.usuario.Opinion;
 
@@ -8,7 +13,7 @@ public class OpinionController {
 
 	public int add(Opinion oOpinion) {
 		String sql = "INSERT INTO opinion VALUES (\"" + oOpinion.getiPuntuacion() + "\",\"" + oOpinion.getsCritica()
-				+ "\",\"" + oOpinion.getiId_opinion()+ ")";
+				+ "\",\"" + oOpinion.getiId_opinion() + ")";
 		return ConexionDB.executeUpdate(sql);
 	}
 
@@ -16,4 +21,29 @@ public class OpinionController {
 		String sql = "DELETE FROM opinion WHERE id_opinion LIKE \"" + oOpinion.getiId_opinion() + "\"";
 		return ConexionDB.executeUpdate(sql);
 	}
+
+	//Operacion de búsqueda por el ID
+	
+	public List<Opinion> buscarOpinionPorId(Opinion oOpinion) {
+
+		List<Opinion> lOpiniones = new ArrayList<Opinion>();
+		String sql = "SELECT * FROM opinion WHERE id=" + oOpinion.getiId_opinion();
+		Statement stm = null;
+
+		try {
+			stm = ConexionDB.getConnection().createStatement();
+			ResultSet rs = stm.executeQuery(sql);
+			while (rs.next()) {
+				int iId_opinion = rs.getInt(1);
+				String sCritica = rs.getString(2);
+				byte bPuntuacion = (byte) rs.getInt(3);
+				lOpiniones.add(new Opinion(iId_opinion,sCritica,bPuntuacion));
+			}
+			stm.close();
+		} catch (SQLException e) {
+			lOpiniones = null;
+		}
+		return lOpiniones;
+	}
+
 }
