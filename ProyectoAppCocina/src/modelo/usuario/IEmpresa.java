5369f58057a562ebcfe73c8174ef24e56ee0a574
Jesu0108 <jartiguez2@gmail.com>
2020-05-04 10:15:44 +0200

    Se ha creado la interfaz IEmpresa y se le ha implementado a la clase Empresa

---
+++ ProyectoAppCocina/src/modelo/usuario/IEmpresa.java
@@ -0,0 +1,23 @@
+package modelo.usuario;
+
+public interface IEmpresa {
+
+	public String getsCif();
+
+	public String getsDomilio();
+
+	public boolean setsDomilio(String sDomilio);
+
+	public String getsCiudad();
+
+	public boolean setsCiudad(String sCiudad);
+
+	public String getsNombre();
+
+	public boolean setsNombre(String sNombre);
+
+	public String getsTelefono();
+
+	public boolean setsTelefono(String sTelefono);
+
+}
