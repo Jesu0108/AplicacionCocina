c20e0ff48c4f2e2a317cd5e95a5519a447eaa7d5
Jesu0108 <jartiguez2@gmail.com>
2020-05-04 14:05:58 +0200

    Se ha creado la clase Catador_x_servicio con sus respectivos metodos

---
+++ ProyectoAppCocina/src/modelo/intermedias/Catador_x_servicio.java
@@ -0,0 +1,54 @@
+package modelo.intermedias;
+
+import modelo.usuario.Catador;
+import modelo.usuario.Servicio;
+
+public class Catador_x_servicio {
+	private Catador email; // PK and FK
+	private Servicio id_servicio; // PK and FK
+
+	public Catador_x_servicio(Catador email, Servicio id_servicio) {
+		setEmail(email);
+		setId_servicio(id_servicio);
+	}
+
+	// Getters and Setters
+
+	public Catador getEmail() {
+		return email;
+	}
+
+	private void setEmail(Catador email) {
+		this.email = email;
+	}
+
+	public Servicio getId_servicio() {
+		return id_servicio;
+	}
+
+	private void setId_servicio(Servicio id_servicio) {
+		this.id_servicio = id_servicio;
+	}
+
+	// -----------------------------------------------------------------------------
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id_servicio == null) ? 0 : id_servicio.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean bExito = false;
+		Catador_x_servicio other = (Catador_x_servicio) obj;
+		if (this.email.equals(other.email) && this.id_servicio.equals(other.id_servicio)) {
+			bExito = true;
+		}
+		return bExito;
+	}
+
+}
