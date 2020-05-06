62e736474fe1a6997e385a01eccb75fe985e4611
Jesu0108 <jartiguez2@gmail.com>
2020-05-06 13:45:57 +0200

    se ha cambiado la funcion AD de PersonaController

--- ProyectoAppCocina/src/controlador/usuario/PersonaController.java
+++ ProyectoAppCocina/src/controlador/usuario/PersonaController.java
@@ -5,27 +5,31 @@ import modelo.usuario.Persona;
 
 public class PersonaController {
 
-	//Operaciones CRUD
-	
-	public int add (Persona oPersona) {
-		String sql = "INSERT INTO persona VALUES (\""+oPersona.getsEmail()+"\",\""+oPersona.getsContrasenia()+"\")";
+	public int add(String sEmail, String sContrasenia, String sNombre, String sApellidos, String sTelefono,
+			String sExperiencia, String sCiudad) {
+
+		String sql = "INSERT INTO catador VALUES (";
+		sql += "\"" + sEmail + "\",";
+		sql += "\"" + sContrasenia + "\",";
+		sql += "\"" + sNombre + "\",";
+		sql += "\"" + sApellidos + "\",";
+		sql += "\"" + sTelefono + "\",";
+		sql += "\"" + sExperiencia + "\",";
+		sql += "\"" + sCiudad + "\",";
+		sql += ")";
+
 		return ConexionDB.executeUpdate(sql);
 	}
-	
-	public int remove (Persona oPersona) {
-		String sql = "DELETE FROM persona WHERE email LIKE \""+oPersona.getsEmail()+"\"";
-		return ConexionDB.executeUpdate(sql);
-	}
-	
-	//Operaciones de checking
-	
-	public int existePersona (Persona oPersona) {
+
+	// Operaciones de checking
+
+	public int existePersona(Persona oPersona) {
 		String sql = "SELECT COUNT(*) FROM persona WHERE email LIKE \"" + oPersona.getsEmail() + "\"";
 		return ConexionDB.executeCount(sql);
-	    }
-	
-	 public int checkLogin(Persona oPersona) {	
-			String sql = "SELECT COUNT(*) FROM persona WHERE email LIKE \"" + oPersona.getsEmail() + "\" AND contraseña LIKE \"" + oPersona.getsContrasenia() + "\"";	
-			return ConexionDB.executeCount(sql);
-		    }
+	}
+
+	public int checkLogin(Persona oPersona) {
+		String sql = "SELECT COUNT(*) FROM persona WHERE email LIKE \"" + oPersona.getsEmail() + "\" AND contraseña LIKE \"" + oPersona.getsContrasenia() + "\"";
+		return ConexionDB.executeCount(sql);
+	}
 }
