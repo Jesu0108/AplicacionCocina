244ca7357dbbe467f843caf3346d525bf6cb2701
Jesu0108 <jartiguez2@gmail.com>
2020-05-06 13:40:00 +0200

    se han hehco ajustes esteticos de codigo en algunas clases

--- ProyectoAppCocina/src/controlador/usuario/CatadorController.java
+++ ProyectoAppCocina/src/controlador/usuario/CatadorController.java
@@ -2,16 +2,29 @@ package controlador.usuario;
 
 import controlador.ConexionDB;
 import modelo.usuario.Catador;
+import modelo.usuario.Persona;
 
 public class CatadorController {
 	
-    public int remove (Catador oCatador) {
+	public int add (Catador oCatador, PersonaController oPersona) {
+		
+		//oPersona.add(oPersona);
+		
+		String sql = "INSERT INTO catador VALUES (";
+		sql += "\"" + oCatador.getsEmail() + "\",";
+		sql += "\"" + oCatador.getCriterio()+ "\",";
+		sql += ")";
+		
+		return ConexionDB.executeUpdate(sql);
+	}
+	
+	public int remove (Catador oCatador) {
 	String sql = "DELETE FROM catador WHERE email LIKE \""+oCatador.getsEmail()+"\"";
 	return ConexionDB.executeUpdate(sql);
     }
 
     // ##########
-    public int existeCliente (Catador oCatador) {
+    public int existeCatador (Catador oCatador) {
 	String sql = "SELECT COUNT(*) FROM catador WHERE email LIKE \"" + oCatador.getsEmail() + "\"";
 	return ConexionDB.executeCount(sql);
     }
