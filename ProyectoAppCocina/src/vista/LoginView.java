3a02988ef610da4e698916ad170b6ec8dfe606a5
Jesu0108 <jartiguez2@gmail.com>
2020-05-07 18:49:17 +0200

    Se ha añadido otra funcion de login en la clase 'LoginView', aun no acabada

--- ProyectoAppCocina/src/vista/LoginView.java
+++ ProyectoAppCocina/src/vista/LoginView.java
@@ -3,6 +3,7 @@ package vista;
 import controlador.ControladorGeneral;
 import modelo.usuario.Catador;
 import modelo.usuario.Cocinero;
+import modelo.usuario.Empresa;
 import validaciones.ValidaLibrary;
 
 public class LoginView {
@@ -78,4 +79,41 @@ public class LoginView {
 		}
 		return oLoginCatador;
 	}
+	
+	//---------------------------------------------------------------------------------------------------------------
+	
+		public static Empresa loginEmpresa(ControladorGeneral controlador) {
+			boolean errorControl = true;
+			String sEmail = null, sContrasenia = null;
+			Empresa oLoginEmpresa = null;
+
+			System.out.println("Por favor, introduzca sus credenciales para poder acceder.");
+			while (errorControl) {
+				try {
+					sEmail = ValidaLibrary.leer("Email: ");
+					errorControl = false;
+				} catch (Exception ex) {
+					System.out.println("Error de email: " + ex.getMessage());
+				}
+			}
+
+			errorControl = true;
+			while (errorControl) {
+				try {
+					sContrasenia = ValidaLibrary.leer("Contrasenia: ");
+					errorControl = false;
+				} catch (Exception ex) {
+					System.out.println("Error de contrasenia: " + ex.getMessage());
+				}
+			}
+
+			Empresa oEmpresa = new Empresa(sEmail, sContrasenia);
+
+			//Comprobamos que existe una empresa con esos datos
+			
+			if (controlador > 0) {
+				oLoginEmpresa = oEmpresa;
+			}
+			return oLoginEmpresa;
+		}
 }
