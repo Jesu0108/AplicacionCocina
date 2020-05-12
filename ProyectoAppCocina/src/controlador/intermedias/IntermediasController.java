6a628fc95246918f0dc6ae6e3c73fdac76bf7fce
Jesu0108 <jartiguez2@gmail.com>
2020-05-12 10:28:32 +0200

    Se ha añadido el controlador de 'Empresa_x_servicio' al controlador de intermedias

--- ProyectoAppCocina/src/controlador/intermedias/IntermediasController.java
+++ ProyectoAppCocina/src/controlador/intermedias/IntermediasController.java
@@ -5,14 +5,16 @@ public class IntermediasController {
 	private Catador_x_servicioController catXservCtrl;
 	private Cocinero_x_servicioController cocXservCtrl;
 	private Material_x_servicioController matXservCtrl;
+	private Empresa_x_servicioController empXservCrtl;
 	
 	public IntermediasController() {
 		alimXservCtrl = new Alimento_x_servicioController();
 		catXservCtrl = new Catador_x_servicioController();
 		cocXservCtrl = new Cocinero_x_servicioController();
 		matXservCtrl = new Material_x_servicioController();
+		empXservCrtl = new Empresa_x_servicioController();
 	}
-
+	
 	public Alimento_x_servicioController getAlimXservCtrl() {
 		return alimXservCtrl;
 	}
@@ -29,5 +31,8 @@ public class IntermediasController {
 	public Material_x_servicioController getMatXservCtrl() {
 		return matXservCtrl;
 	}
-	
+
+	public Empresa_x_servicioController getEmpXservCrtl() {
+		return empXservCrtl;
+	}
 }
